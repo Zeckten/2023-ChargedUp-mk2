0aacdaf6e03e3ecd8a39231583f1ca5ddae1f121
Zeckten <64096716+Zeckten@users.noreply.github.com>
2023-02-04 15:51:20 -0800

    RotateToTarget with kAimButton

--- src/main/java/com/spartronics4915/frc2023/RobotContainer.java
+++ src/main/java/com/spartronics4915/frc2023/RobotContainer.java
@@ -95,8 +95,9 @@ public class RobotContainer {
                     new JoystickButton(mController, kResetOdometryButton)
                     .onTrue(mSwerveCommands.new ResetOdometry());
 
-                    // new JoystickButton(mController, kAimButton)
-                    // .onTrue(mSwerveCommands.new RotateDegrees(cameraWrapper.photonCamera.getLatestResult().getBestTarget().getYaw()));
+                    new JoystickButton(mController, kAimButton)
+                    .onTrue(mSwerveCommands.new RotateToTarget(cameraWrapper));
+                    // cameraWrapper.photonCamera.getLatestResult().getBestTarget().getYaw()
                 }
             }
             
